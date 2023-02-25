991f88ea2be363aed4402ef6fe82e3b2f82e6b54
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-02-25 23:43:57 +0530

    Create Update&Delete.sql

---
+++ Update&Delete.sql
@@ -0,0 +1,9 @@
+SELECT * FROM student1;
+UPDATE student1
+SET major='Electronics'
+WHERE major='ECE';
+UPDATE student1
+SET major='Electrical'
+WHERE major='EEE';
+DELETE FROM student1
+WHERE student_name='Rumi';
